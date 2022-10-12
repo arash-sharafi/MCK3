dac16480d9e81a742e6a7bfb1676459e1bc0ee6c
Administrator <arash.sharafi.art@gmail.com>
2022-10-12 18:16:00 -0400

    Initial Commit

---
+++ Mock3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mock3.Startup))]
+namespace Mock3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
