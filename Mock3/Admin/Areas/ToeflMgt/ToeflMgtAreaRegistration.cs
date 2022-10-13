038b46dc50e4e934bbe4193df10ae3eb9c2a5e30
Administrator <arash.sharafi.art@gmail.com>
2022-10-13 14:41:31 -0400

    Create Admin Area

---
+++ Mock3/Admin/Areas/ToeflMgt/ToeflMgtAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Mock3.Areas.ToeflMgt
+{
+    public class ToeflMgtAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ToeflMgt";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ToeflMgt_default",
+                "ToeflMgt/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
