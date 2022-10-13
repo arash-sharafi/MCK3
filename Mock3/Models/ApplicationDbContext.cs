85beade14907fcd506cf0d883392ba38de33d1dd
Administrator <arash.sharafi.art@gmail.com>
2022-10-13 14:13:00 -0400

    Create Voucher Model

--- Mock3/Models/ApplicationDbContext.cs
+++ Mock3/Models/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace Mock3.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Voucher> Vouchers { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
---
+++ Mock3/Models/Voucher.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mock3.Models
+{
+    public class Voucher
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(16)]
+        public string VoucherNo { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string CreateDate { get; set; }
+        
+
+        [Required]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+    }
+}
