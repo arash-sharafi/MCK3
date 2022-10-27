32046aaed50cd99e7969a948e0c53c0e724f114f
Administrator <arash.sharafi.art@gmail.com>
2022-10-27 16:38:54 -0400

    Improving User Register and Login UI and Validation

--- Mock3/Models/AccountViewModels.cs
+++ Mock3/Models/AccountViewModels.cs
@@ -48,55 +48,55 @@ namespace Mock3.Models
 
     public class LoginViewModel
     {
-        [Required]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage = "لطفا ایمیل را وارد کنید")]
+        [Display(Name = "ایمیل")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "لطفا رمز عبور را وارد کنید")]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "رمز عبور")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "به خاطر بسپار")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage = "لطفا ایمیل را وارد کنید")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده درست نیست")]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "لطفا رمز عبور را وارد کنید")]
+        [StringLength(100, ErrorMessage = "حداقل طول  {0}، {2} رقم می باشد.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "رمز عبور")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "تکرار رمز عبور")]
+        [Compare("Password", ErrorMessage = "رمز عبور و تکرار آن باید یکسان باشند.")]
         public string ConfirmPassword { get; set; }
     }
 
     public class ResetPasswordViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "لطفا ایمیل را وارد کنید")]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "حداقل طول  {0}، {2} رقم می باشد.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "رمز عبور و تکرار آن باید یکسان باشند.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
