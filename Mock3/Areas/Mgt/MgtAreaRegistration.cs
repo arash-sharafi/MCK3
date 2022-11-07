b97c5603af2940b1c19657f1bc0bd3db1ae821ff
Administrator <arash.sharafi.art@gmail.com>
2022-11-07 12:44:44 -0500

    Update Admin area scaffolding

---
+++ Mock3/Areas/Mgt/MgtAreaRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Mock3.Areas.Mgt
+{
+    public class MgtAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName => "Mgt";
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Mgt_default",
+                "Mgt/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
