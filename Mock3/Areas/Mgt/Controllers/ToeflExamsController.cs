baaa8142d95cdd77e915220e0e631940485e12e9
Administrator <arash.sharafi.art@gmail.com>
2022-11-08 10:44:26 -0500

    Implement Admin CRUD For Exam

---
+++ Mock3/Areas/Mgt/Controllers/ToeflExamsController.cs
@@ -0,0 +1,106 @@
+﻿using Mock3.Areas.Mgt.ViewModels;
+using Mock3.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Mock3.Areas.Mgt.Controllers
+{
+    public class ToeflExamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToeflExamsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Mgt/ToeflExams
+        public ActionResult Index()
+        {
+            var exams = _context
+                .Exams
+                .OrderBy(x => x.StartDate)
+                .ToList();
+            return View(exams);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ExamMgtViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            _context.Exams.Add(new Exam
+            {
+                Name = model.Name,
+                StartDate = model.StartDate,
+                Capacity = model.Capacity,
+                Description = model.Description,
+                IsOpen = model.IsOpen
+            });
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var exam = _context.Exams.Find(id);
+
+            if (exam == null) return RedirectToAction("Index");
+
+
+            var model = new ExamMgtViewModel
+            {
+                Name = exam.Name,
+                StartDate = exam.StartDate,
+                Description = exam.Description,
+                Capacity = exam.Capacity,
+                IsOpen = exam.IsOpen
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ExamMgtViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+
+            var exam = _context.Exams.Find(model.Id);
+
+            if (exam == null) return RedirectToAction("Index");
+
+            exam.Name = model.Name;
+            exam.Capacity = model.Capacity;
+            exam.Description = model.Description;
+            exam.StartDate = model.StartDate;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var exam = _context.Exams.Find(id);
+
+            if (exam == null) return RedirectToAction("Index");
+
+            _context.Exams.Remove(exam);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Mock3/Areas/Mgt/ViewModels/ExamMgtViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mock3.Areas.Mgt.ViewModels
+{
+    public class ExamMgtViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "عنوان آزمون را وارد کنید")]
+        [StringLength(100)]
+        [Display(Name="عنوان آزمون")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "تاریخ آزمون را وارد کنید")]
+        [StringLength(10)]
+        [Display(Name="تاریخ آزمون")]
+        public string StartDate { get; set; }
+
+        [StringLength(300)] 
+        [Display(Name="توضیحات آزمون")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "ظرفیت آزمون را وارد کنید")]
+        [Display(Name="ظرفیت آزمون")]
+        public int Capacity { get; set; }
+        
+        public int RemainingCapacity { get; set; }
+
+        public bool IsOpen { get; set; }
+
+    }
+}
